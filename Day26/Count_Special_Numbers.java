5e2f518eab26538dc0a107ac3ddb77c46452caea
rajdipahir89 <122194637+rajdipahir89@users.noreply.github.com>
2023-04-05 23:36:21 +0530

    Add files via upload

---
+++ Day26/Count_Special_Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSpecialNumbers(int N, int arr[]) {
+        // Code here
+        int count = 0;
+        int ans = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            if (i < N - 1 && arr[i]==arr[i+1]) {
+                count++;
+            } else {
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] % arr[j] == 0) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
